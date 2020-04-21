48285a7cab3dc0a55980cf279e5c1be48ffd9e52
AndrewZapotochnyi <info@azblockchain.co>
2020-04-21 22:21:23 +0000

    Tweet CSS created

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+  $('.new-tweet form textarea').on('keyup', function() {
+    let currentCount = 140 - this.value.length;
+
+    let counter = $(this).next().next();
+
+    if (currentCount < 0) {
+      counter.css("color", "red");
+    } else if (currentCount > 0) {
+      counter.css("color", "black");
+    }
+    
+    $(counter).text(currentCount);
+  });
+
+});
+
+
+
